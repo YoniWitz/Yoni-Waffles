825a6c02f80535ace859ced4d86541d04747d44c
Yoni Hirshowitz <jonhirsh39@gmail.com>
2019-11-21 12:43:24 -0500

    cleaned project

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+  
+<template>
+  <div class="navbar">
+    <nav class="nav-extended indogo darken-2">
+      <div class="nav-content">
+        <router-link to>
+          <span class="nav-title">Yoni Waffles</span>
+        </router-link>
+        <a href class="btn-floating btn-larger halfway-fab pink">
+          <router-link to>
+            <i class="material-icons">add</i>
+          </router-link>
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbar"
+};
+</script>
+
+<style scoped>
+.navbar nav {
+    padding: 0 20px;
+}
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="index">
+     <h1>"Welcome to Your Vue.js App"</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data(){
+  
+  }
+}
+</script>
