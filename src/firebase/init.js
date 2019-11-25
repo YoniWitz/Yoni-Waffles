7fd2e5da15face26d5c3b9cce15e5fa4559c49fa
Yoni Hirshowitz <jonhirsh39@gmail.com>
2019-11-25 10:19:12 -0500

    deployed to firebase

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,13 +2,13 @@
  import firestore from 'firebase/firestore'
  
  var firebaseConfig = {
-  apiKey: "AIzaSyCdry8NA3Cv39sX8kKbF0zcZMBsdR7RPrY",
-  authDomain: "fast-tensor-697.firebaseapp.com",
-  databaseURL: "https://fast-tensor-697.firebaseio.com",
-  projectId: "fast-tensor-697",
-  storageBucket: "fast-tensor-697.appspot.com",
-  messagingSenderId: "300761388332",
-  appId: "1:300761388332:web:37251d5dfbef099434c889"
+  apiKey: "AIzaSyBtCTuZH25isuRsMN0HxpBWErscsFfALao",
+  authDomain: "yonis-waffles.firebaseapp.com",
+  databaseURL: "https://yonis-waffles.firebaseio.com",
+  projectId: "yonis-waffles",
+  storageBucket: "yonis-waffles.appspot.com",
+  messagingSenderId: "495772541801",
+  appId: "1:495772541801:web:5653bc7e748da9995213f8"
 };
   // Initialize Firebase
  const firebaseApp = firebase.initializeApp(firebaseConfig);
